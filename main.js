3780d82b39e33ed9b57531e7b4dcd5b223ca9878
muh <muh@Jemy.local>
2023-02-24 23:17:57 +0300

    Add visitor count, fix up internal links.

--- main.js
+++ main.js
@@ -5,3 +5,18 @@ navList = document.querySelector(".nav-list");
 hamburger.onclick = function () {
   navList.classList.toggle("active");
 };
+
+// Some code for the visitors count. I am using count api with a get request to get the number of visitors. Every time the page loads, this script is ran once, updating the visitor count.
+
+const visitors = document.getElementById("visitors");
+// Call the function defined bellow
+updateVisitCount();
+
+// Create a function to fetch the value of the visitor count from the count api.
+function updateVisitCount() {
+  fetch("https://api.countapi.xyz/update/maverick-dresses/visitors/?amount=1")
+    .then((res) => res.json())
+    .then((res) => {
+      visitors.innerHTML = res.value;
+    });
+}
